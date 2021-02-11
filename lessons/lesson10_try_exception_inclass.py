1bebb3d6a2aba27176cb8e63c6fd981089aff3a2
Eldar Supataev <eldarnyc@gmail.com>
2021-02-12 01:42:48 +0600

    try_exception

--- lessons/lesson10_try_exception_inclass.py
+++ lessons/lesson10_try_exception_inclass.py
@@ -1,18 +1,1 @@
-import numpy as np
-from scipy.spatial import Delaunay
-import matplotlib.pyplot as plt
-
-points = np.array([
-  [2, 4],
-  [3, 4],
-  [3, 0],
-  [2, 2],
-  [4, 1]
-])
-
-simplices = Delaunay(points).simplices
-
-plt.triplot(points[:, 0], points[:, 1], simplices)
-plt.scatter(points[:, 0], points[:, 1], color='r')
-
-plt.show()
+# TRY - EXCEPT - FINALLY - RAISE
